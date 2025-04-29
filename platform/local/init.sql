9ef6344d97574b44e0c474874f943b869cd37800
mark <mwashburn160@gmail.com>
2025-04-29 09:07:17 -0500

    chore: cleanup

--- platform/local/init.sql
+++ platform/local/init.sql
@@ -1,7 +1,9 @@
 \connect pipeline_builder
 create table if not exists pipeline_builder (
     id uuid primary key default gen_random_uuid(),
+    created_by text not null default current_user,
     created_at timestamp not null default current_timestamp,
+    updated_by text not null default current_user,
     updated_at timestamp not null default current_timestamp,
     description text,
     is_active boolean default true
