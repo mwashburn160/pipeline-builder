8ed1cb2ff2b48a95b5b2b919d1919d1c956c1a58
mark <mwashburn160@gmail.com>
2025-04-29 08:54:46 -0500

    chore: cleanup

--- platform/local/init.sql
+++ platform/local/init.sql
@@ -1,8 +1,5 @@
-create role pipeline_builder with superuser login password 'password'
 \connect pipeline_builder
-
-create database pipeline_builder with owner pipeline_builder
-create table pipeline_builder(
+create table if not exists pipeline_builder (
     id uuid primary key default gen_random_uuid(),
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
