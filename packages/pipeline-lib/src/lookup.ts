a3c5163543b0579e5edfd5e28e8e7c71a60dc73e
mwashburn160 <mwashburn160@gmail.com>
2025-07-12 07:24:12 -0500

    chore: cleanup

--- packages/pipeline-lib/src/lookup.ts
+++ packages/pipeline-lib/src/lookup.ts
@@ -7,55 +7,82 @@ import { UniqueId } from './unique-id';
 import { PluginConfig } from './plugin-config';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
+/**
+ * A construct that creates a custom resource to fetch PluginConfig data using a Lambda-backed provider.
+ */
 export class Lookup extends Construct {
-  private _uniqueId: UniqueId;
-  private _provider: Provider;
-  private _config: PluginConfig
+    private readonly _uniqueId: UniqueId;
+    private readonly _provider: Provider;
+    private _config: PluginConfig;
 
-  constructor(scope: Construct, id: string, organization: string, project: string) {
-    super(scope, id);
-    this._config = {
-      pluginName: 'no_pluginname',
-      pluginType: 'CodeBuildStep',
-      version: '1.0.0',
-      commands: []
+    /**
+     * @param scope - The parent construct.
+     * @param id - The construct ID.
+     * @param organization - The organization name.
+     * @param project - The project name.
+     */
+    constructor(scope: Construct, id: string, organization: string, project: string) {
+        super(scope, id);
+        if (!organization || !project) {
+            throw new Error('organization and project are required');
+        }
+
+        this._config = {
+            pluginName: 'no_pluginname',
+            pluginType: 'CodeBuildStep',
+            version: '1.0.0',
+            commands: [],
+        };
+        this._uniqueId = new UniqueId(organization, project);
+        const onEventHandler = new NodejsFunction(this, this._uniqueId.generate('onevent-handler'), {
+            runtime: Constants.NODEJS_VERSION,
+            timeout: Constants.DEFAULT_TIMEOUT,
+            memorySize: Constants.DEFAULT_MEMORY_SIZE,
+            architecture: Constants.DEFAULT_ARCHITECTURE,
+            entry: Constants.LAMBDA_ENTRY_PATH || `${__dirname}/custom-resource/index.ts`,
+        });
+
+        onEventHandler.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+            resources: [`arn:aws:logs:${this.stack.region}:${this.stack.account}:log-group:/aws/lambda/${onEventHandler.functionName}:*`],
+        }));
+
+        this._provider = new Provider(this, this._uniqueId.generate('resource-provider'), {
+            onEventHandler,
+            logRetention: Constants.DEFAULT_LOG_RETENTION,
+        });
     }
-    this._uniqueId = new UniqueId(organization, project);
-    let onEventHandler = new NodejsFunction(this, this._uniqueId.generate('onevent-handler'), {
-      runtime: Constants.NODEJS_VERSION,
-      timeout: Constants.DEFAULT_TIMEOUT,
-      memorySize: Constants.DEFAULT_MEMORY_SIZE,
-      architecture: Constants.DEFAULT_ARCHITECTURE,
-      entry: `${__dirname}/custom-resource/index.ts`,
-    });
 
-    onEventHandler.addToRolePolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
-      resources: ['*']
-    }))
-    this._provider = new Provider(this, this._uniqueId.generate('resource-provider'), {
-      onEventHandler: onEventHandler,
-      logRetention: Constants.DEFAULT_LOG_RETENTION
-    });
-  }
-
-  config(pluginName: string): PluginConfig {
-    try {
-      let custom = new CustomResource(this, this._uniqueId.generate(pluginName), {
-        serviceTimeout: Constants.DEFAULT_TIMEOUT,
-        serviceToken: this._provider.serviceToken,
-        resourceType: "Custom::PluginConfig",
-        properties: {
-          pluginName: pluginName,
-        },
-      });
-      if (!Token.isUnresolved(custom.getAttString('PluginConfig'))) {
-        this._config = JSON.parse(custom.getAttString('PluginConfig'))
-      }
-    } catch (error) {
-      console.log(error)
+    /**
+     * Fetches a PluginConfig for the given pluginName using a custom resource.
+     * @param pluginName - The name of the plugin to fetch configuration for.
+     * @returns The PluginConfig object.
+     * @throws Error if the configuration cannot be fetched or parsed.
+     */
+    config(pluginName: string): PluginConfig {
+        if (!pluginName) {
+            throw new Error('pluginName is required');
+        }
+        try {
+            const custom = new CustomResource(this, this._uniqueId.generate(pluginName), {
+                serviceTimeout: Constants.DEFAULT_TIMEOUT,
+                serviceToken: this._provider.serviceToken,
+                resourceType: 'Custom::PluginConfig',
+                properties: { pluginName },
+            });
+            const pluginConfig = custom.getAttString('PluginConfig');
+            if (!Token.isUnresolved(pluginConfig)) {
+                this._config = JSON.parse(pluginConfig);
+                if (!this._config.commands?.length) {
+                    throw new Error(`PluginConfig for ${pluginName} has no commands`);
+                }
+            } else {
+                throw new Error(`PluginConfig for ${pluginName} is unresolved during synthesis`);
+            }
+        } catch (error) {
+            throw new Error(`Failed to fetch PluginConfig for ${pluginName}: ${error}`);
+        }
+        return this._config;
     }
-    return this._config
-  }
 }
