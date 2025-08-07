b72408c91b819a628f4613443a818898b04998a2
mark <mwashburn160@gmail.com>
2025-08-06 19:28:04 -0500

    chore: cleanup

--- api/backend/src/functions/add-plugin/src/index.ts
+++ api/backend/src/functions/add-plugin/src/index.ts
@@ -1,21 +1,5 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        const message = 'Hello World....';
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-    } catch (error) {
-        console.error('Error:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error' }),
-        };
-    }
-};
+export class Hello {
+  public sayHello() {
+    return 'hello, world!';
+  }
+}
