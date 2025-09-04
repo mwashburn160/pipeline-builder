16fd5df0a60d8a61d1bb01c3ba13609ffecb22e3
mark <mwashburn160@gmail.com>
2025-09-04 09:50:22 -0500

    chore: docker cleanup

--- projenrc/function.ts
+++ projenrc/function.ts
@@ -15,7 +15,7 @@ export class FunctionProject extends TypeScriptAppProject {
         execSync(`if [ ! -d ${this._home} ];then mkdir -p ${this._home};fi`)
         this.addScripts({
             'docker:build': `docker build -t ${this.name}:$\{IMAGE_TAG:-latest\} .`,
-            'docker:run': `docker run ${this.name}:$\{IMAGE_TAG:-latest\} .`
+            'docker:run': `docker run ${this.name}:$\{IMAGE_TAG:-latest\}`
         })
     }
 
