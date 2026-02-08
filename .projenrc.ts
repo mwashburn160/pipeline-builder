eee2516d0349d57617d568e37beb162fe09ae9db
mark <mwashburn160@gmail.com>
2026-02-08 15:55:05 -0600

    fix: upgraded api-server=1.6.23

--- .projenrc.ts
+++ .projenrc.ts
@@ -55,7 +55,7 @@ let expressVersion = '5.2.1'
 let apiCoreVersion = '1.8.17';
 
 /** @mwashburn160/api-server package version */
-let apiServerVersion = '1.6.20'; 
+let apiServerVersion = '1.6.23'; 
 
 /** @mwashburn160/pipeline-data package version */
 let pipelineDataVersion = '1.9.8';
