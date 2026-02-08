a52d16d8383f975134007d89f830c16f071d6bea
mark <mwashburn160@gmail.com>
2026-02-08 11:43:04 -0600

    fix: upgraded pipeline-core=1.8.6

--- .projenrc.ts
+++ .projenrc.ts
@@ -61,7 +61,7 @@ let apiServerVersion = '1.6.15';
 let pipelineDataVersion = '1.9.5';
 
 /** @mwashburn160/pipeline-core package version */
-let pipelineCoreVersion = '1.8.4';
+let pipelineCoreVersion = '1.8.6';
 
 // =============================================================================
 // Root Project Configuration
