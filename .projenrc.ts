4154d7cfb8d93b61dcbd955e1dc2bad176a40075
mark <mwashburn160@gmail.com>
2026-02-07 20:43:52 -0600

    fix: api-server version=1.6.6

--- .projenrc.ts
+++ .projenrc.ts
@@ -55,7 +55,7 @@ let expressVersion = '5.2.1'
 let apiCoreVersion = '1.8.4';
 
 /** @mwashburn160/api-server package version */
-let apiServerVersion = '1.6.1';
+let apiServerVersion = '1.6.6';
 
 /** @mwashburn160/pipeline-data package version */
 let pipelineDataVersion = '1.8.4';
