3a41c948044de84720a669c2fc39bd74b6afec82
mark <mwashburn160@gmail.com>
2026-02-25 11:53:04 -0600

    fix: updated workflow

--- projenrc/workflow.ts
+++ projenrc/workflow.ts
@@ -123,7 +123,7 @@ export class Workflow extends Component {
                     name: 'Derive SHAs for NX:BASE, NX:HEAD',
                     uses: 'nrwl/nx-set-shas@v4',
                     with: {
-                        'workflow-id': 'workflow.yml',
+                        'workflow-id': '${{ github.workspace }}/workflow.yml',
                         'main-branch-name': 'main'
                     },
                 },
