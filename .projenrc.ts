4d258a0729f49211e163f358804f837fe130b874
mark <mwashburn160@gmail.com>
2026-03-02 20:03:18 -0600

    fix: upgraded ai-core=1.7.4

--- .projenrc.ts
+++ .projenrc.ts
@@ -19,13 +19,13 @@ const cdkVersion = '2.240.0';
 const expressVersion = '5.2.1';
 
 // Internal package versions — use workspace:* for local, or pin e.g. '1.6.6' for npm
-const ws = 'workspace:*';
+///const ws = 'workspace:*';
 const pkg = {
-  apiCore:      '1.33.3',
-  pipelineData: '1.34.3',
-  pipelineCore: '1.34.3',
-  apiServer:    '1.32.3',
-  aiCore:       ws,
+  apiCore:      '1.33.4',
+  pipelineData: '1.34.4',
+  pipelineCore: '1.34.4',
+  apiServer:    '1.32.4',
+  aiCore:       '1.7.4',
 };
 
 // -- Root project --
