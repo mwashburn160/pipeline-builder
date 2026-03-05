f77a0cfe0c65aeac6cf48ec5db7c62540155c485
mark <mwashburn160@gmail.com>
2026-03-05 15:09:36 -0600

    fix: upgraded api-server=1.35.3

--- .projenrc.ts
+++ .projenrc.ts
@@ -21,10 +21,10 @@ const expressVersion = '5.2.1';
 // Internal package versions — use workspace:* for local, or pin e.g. '1.6.6' for npm
 const ws = 'workspace:*';
 const pkg = {
-  apiCore:      '1.36.2',
-  pipelineData: '1.37.2',
-  pipelineCore: '1.37.2',
-  apiServer:    ws,
+  apiCore:      '1.36.3',
+  pipelineData: '1.37.3',
+  pipelineCore: '1.37.3',
+  apiServer:    '1.35.3',
   aiCore:       ws,
 };
 
