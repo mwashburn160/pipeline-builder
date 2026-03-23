2735eb8d998f27747feec5b507d8d5f404f52875
mark <mwashburn160@gmail.com>
2026-03-23 15:42:18 -0500

    fix: version=1.42.15

--- .projenrc.ts
+++ .projenrc.ts
@@ -21,8 +21,8 @@ const expressVersion = '5.2.1';
 // Internal package versions — use workspace:* for local, or pin e.g. '1.6.6' for npm
 const ws = 'workspace:*';
 const pkg = {
-  apiCore:        '1.42.14',
-  pipelineData:   ws,
+  apiCore:        '1.42.15',
+  pipelineData:   '1.43.15',
   pipelineCore:   ws,
   apiServer:      ws,
   aiCore:         ws,
