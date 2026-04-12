38ec64fdc66e8e3afc99475a5813a6aa7d620fa6
mark <mwashburn160@gmail.com>
2026-04-12 14:58:46 -0500

    fix: upgraded pipelineData=1.50.15

--- .projenrc.ts
+++ .projenrc.ts
@@ -27,8 +27,8 @@ const expressVersion = '5.2.1';
 // Internal package versions — use workspace:* for local, or pin for npm
 //const ws = 'workspace:*';
 const pkg = {
-  apiCore:        '1.49.14',
-  pipelineData:   '1.50.14',
+  apiCore:        '1.49.15',
+  pipelineData:   '1.50.15',
   pipelineCore:   '1.50.15',
   apiServer:      '1.46.14',
   aiCore:         '1.21.14',
